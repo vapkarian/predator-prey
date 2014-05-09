6c369b4f693cb4cf501b251a3ee70627be034e96
Victor Apkarian <victor.apkarian@bodugi.com>
2014-05-09 14:17:37 +0300

    rendering optimization

--- gui.py
+++ gui.py
@@ -9,71 +9,54 @@ VICTIM_COLOR = 'green'
 TIMER = 100
 
 
-class Cell(Tkinter.Frame):
-    def __init__(self, window, master, x, y):
-        Tkinter.Frame.__init__(self, master, width=CELL_SIZE, height=CELL_SIZE,
-                               background=EMPTY_COLOR)
-        self.config(borderwidth=1, relief=Tkinter.GROOVE)
-        self.grid(row=y, column=x)
-        self.window = window
-        self.x = x
-        self.y = y
-
-    def hunt(self, event):
-        self.config(background=EMPTY_COLOR)
-        for predator in self.window.world.predators[:]:
-            if predator.position == (self.x, self.y):
-                self.window.world.predators.remove(predator)
-                return
-        for victim in self.window.world.victims[:]:
-            if victim.position == (self.x, self.y):
-                self.window.world.victims.remove(victim)
-                return
-
-
 class Window(Tkinter.Tk):
-
     def __init__(self):
         Tkinter.Tk.__init__(self)
-        self.lock = False
-        self.bind(PAUSE_KEY, self.lock_wait)
-        self.root = Tkinter.Frame(self, width=core.M * CELL_SIZE, height=core.N * CELL_SIZE)
-        self.root.pack()
-        self.root.pack_propagate(0)
-        self.title('Predators and victims')
-        self.cells = []
-        for x in xrange(core.M):
-            col = []
-            for y in xrange(core.N):
-                cell = Cell(self, self.root, x, y)
-                cell.bind('<Button-1>', cell.hunt)
-                col.append(cell)
-            self.cells.append(col)
         self.world = core.World()
+        self.title('Predator-prey simulation')
+        self.resizable(width=Tkinter.FALSE, height=Tkinter.FALSE)
+        self.bind(PAUSE_KEY, self.pause)
+        self.canvas = Tkinter.Canvas(self, width=core.M * CELL_SIZE, height=core.N * CELL_SIZE, background=EMPTY_COLOR)
+        self.canvas.bind('<Button-1>', self.click)
+        self.canvas.pack()
+        self.lock = False
         self.timer_update()
 
-    def lock_wait(self, event):
+    def pause(self, event):
         self.lock = not self.lock
         if not self.lock:
             self.timer_update()
 
+    def click(self, event):
+        point = (event.x / CELL_SIZE, event.y / CELL_SIZE)
+
+        for predator in self.world.predators[:]:
+            if predator.position == point:
+                self.world.predators.remove(predator)
+                self.create_cell(point, EMPTY_COLOR)
+                return
+        for victim in self.world.victims[:]:
+            if victim.position == point:
+                self.world.victims.remove(victim)
+                self.create_cell(point, EMPTY_COLOR)
+                return
+
+    def create_cell(self, point, color):
+        x, y = point
+        bbox = (x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE)
+        self.canvas.create_rectangle(*bbox, tags=point, fill=color, width=0, activewidth=1)
+
     def timer_update(self):
         if self.lock:
             return
-
-        for elem in self.world.victims + self.world.predators:
-            self.cells[elem.x][elem.y].config(background=EMPTY_COLOR)
-
+        self.canvas.delete(Tkinter.ALL)
         self.world.cycle()
-
         for victim in self.world.victims:
-            self.cells[victim.x][victim.y].config(background=VICTIM_COLOR)
-
+            self.create_cell(victim.position, color=VICTIM_COLOR)
         for predator in self.world.predators:
-            self.cells[predator.x][predator.y].config(background=PREDATOR_COLOR)
-
+            self.create_cell(predator.position, color=PREDATOR_COLOR)
         if len(self.world.predators):
-            self.root.after(TIMER, self.timer_update)
+            self.after(TIMER, self.timer_update)
         else:
             self.destroy()
 
